c3a05f2884cbb5685ea12ee4e3c20c8b5509eb94
Niels Leenheer <niels@leenheer.nl>
2025-01-06 22:12:52 +0100

    Add UMD and ES browser builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,35 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+
 export default [
+	{
+		input: 'src/main.js',
+		output: {
+			name: 'BarcodeParser',
+			file: 'dist/barcode-parser.umd.js',
+			format: 'umd'
+		},
+		plugins: [
+			resolve(), 
+			commonjs(),
+            terser() 
+		]
+	},
+
+	{
+		input: 'src/main.js',
+		output: { 
+			file: 'dist/barcode-parser.esm.js', 
+			format: 'es' 
+		},
+		plugins: [
+			resolve(),
+			commonjs(),
+            terser()
+		]
+	},
+
 	{
 		input: 'src/main.js',
 		output: [
